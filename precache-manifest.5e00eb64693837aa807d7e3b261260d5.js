922f8bbe6a1688e57172fb9f0260e5312759063f
Ismail-Saleh <ismailsl487@gmail.com>
2020-01-31 21:48:34 +0700

    Updates

---
+++ precache-manifest.5e00eb64693837aa807d7e3b261260d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "151018b6a3592523402bd83b3ce072ad",
+    "url": "/dumb-tick/index.html"
+  },
+  {
+    "revision": "8f705e91ce9bd16935c6",
+    "url": "/dumb-tick/static/css/main.a6fe63db.chunk.css"
+  },
+  {
+    "revision": "774342d1c4fd8ee1c699",
+    "url": "/dumb-tick/static/js/2.629ee5be.chunk.js"
+  },
+  {
+    "revision": "ac7cd4b98255753b2e714ed39f41947f",
+    "url": "/dumb-tick/static/js/2.629ee5be.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f705e91ce9bd16935c6",
+    "url": "/dumb-tick/static/js/main.9c155187.chunk.js"
+  },
+  {
+    "revision": "9087c0af5018b8be9493",
+    "url": "/dumb-tick/static/js/runtime-main.05a0b716.js"
+  }
+]);
